ec81856685ece1826ccc4df00bd71d718706d0fa
Geoff Gravitate <geoff-grav@users.noreply.github.com>
2015-10-14 16:41:49 -0700

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,22 @@
 <?php
 
-echo 'Hello';
+echo 'Hello<br><br>';
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+
+$m = new Memcached();
+
+$servers = array(
+    array('52.88.250.80', 11211, 33),
+    array('52.17.214.233', 11211, 67)
+);
+$m->addServers($servers);
+
+if(!empty($_GET['gg']))
+{
+    $m->set('gg', $_GET['gg']);
+}
+
+echo $m->get('gg');
 
