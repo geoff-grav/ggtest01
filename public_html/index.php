201ca055f5c5e0c1961136d66c5b2f9b6e5908b0
Geoff Gravitate <geoff-grav@users.noreply.github.com>
2015-10-15 16:11:06 -0700

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -10,25 +10,35 @@ ini_set('display_errors', 1);
 $mc = new Memcached('mc');
 $mc->setOption(Memcached::OPT_LIBKETAMA_COMPATIBLE, true);
 
-if($_SERVER['HTTP_HOST'] == '52.88.213.113')
+if($_SERVER['HTTP_HOST'] == '52.88.213.113') //US
 {
     echo 'SERVER A<br>';
   $servers = array(
-    array('52.17.214.233', 11211) // EU
+    //array('52.17.214.233', 11211) // EU
     //array('172.31.20.208', 11211) // US
-    //array('52.91.169.15', 11211) // NV
+    array('52.91.169.15', 11211) // NV
     //array('52.91.157.18', 11211) // NV c4
-  );    
+  );
 }
-else
+else if($_SERVER['HTTP_HOST'] == '52.64.117.146') //AU
 {
     echo 'SERVER B<br>';
   $servers = array(
-    array('172.31.20.86', 11211) // EU
-    //array('52.88.250.80', 11211) // US
-    //array('52.91.169.15', 11211) // NV
+    //array('52.17.214.233', 11211) // EU
+    //array('172.31.20.208', 11211) // US
+    array('52.91.169.15', 11211) // NV
     //array('52.91.157.18', 11211) // NV c4
-  );    
+  );
+}
+else //UK
+{
+    echo 'SERVER C<br>';
+  $servers = array(
+    //array('172.31.20.86', 11211) // EU
+    //array('52.88.250.80', 11211) // US
+    array('52.91.169.15', 11211) // NV
+    //array('52.91.157.18', 11211) // NV c4
+  );
 }
 
 if (!count($mc->getServerList())) 
