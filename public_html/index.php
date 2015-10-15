d9a47767fa647d215012b2cc9d5eb01cbf346da6
Geoff Gravitate <geoff-grav@users.noreply.github.com>
2015-10-14 22:23:59 -0700

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -8,15 +8,17 @@ ini_set('display_errors', 1);
 $m = new Memcached();
 
 $servers = array(
-    array('52.88.250.80', 11211, 33),
-    array('52.17.214.233', 11211, 67)
+    array('52.88.250.80', 11211, 10),
+    array('52.17.214.233', 11211, 90)
 );
 $m->addServers($servers);
 
 if(!empty($_GET['gg']))
 {
     $m->set('gg', $_GET['gg']);
+    $m->set('ip', $_SERVER['REMOTE_ADDR']);
 }
 
-echo $m->get('gg');
+echo $m->get('gg').'<br>';
+echo $m->get('ip');
 
